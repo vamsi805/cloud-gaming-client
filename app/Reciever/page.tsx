039098faf0741078c486647325b086289a0f7bb1
vamsi805 <vamsibobba805@gmail.com>
2026-01-26 17:12:10 +0530

    First Commit

---
+++ app/Reciever/page.tsx
@@ -0,0 +1,52 @@
+"use client"
+import { useEffect,useRef } from "react";
+
+export default function Reciever() {
+const wsRef  = useRef<WebSocket|null>(null);
+ const rtcRef = useRef<RTCPeerConnection|null>(null);
+  const remoteVideoRef = useRef<HTMLVideoElement|null>(null);
+   useEffect(()=>{
+     wsRef.current = new WebSocket('ws://192.168.1.6:8080')
+     rtcRef.current = new RTCPeerConnection();
+     rtcRef.current.onicecandidate = (event) => {if (event.candidate) {
+                wsRef.current?.send(JSON.stringify({ type: 'iceCandidate', candidate: event.candidate }));
+            }}
+ wsRef.current.onopen= () =>{
+   wsRef.current?.send(JSON.stringify({type : 'reciever'}));
+ };
+ wsRef.current.onmessage = async (event:any) =>{
+  console.log(event.data)
+  const message = JSON.parse(event.data);
+  if(message.type === 'createOffer'){
+    rtcRef.current?.setRemoteDescription(message.sdp);
+    const answer = await rtcRef.current?.createAnswer();
+    rtcRef.current?.setLocalDescription(answer);
+    wsRef.current?.send(JSON.stringify({type :'createAnswer',sdp:answer}));
+  }
+  else if(message.type === 'iceCandidate'){
+    rtcRef.current?.addIceCandidate(new RTCIceCandidate(message.candidate));
+  }
+
+ }
+ rtcRef.current.ontrack = (event) =>{
+if (remoteVideoRef.current) {
+  console.log("TRACK DETECTED")
+  console.log(event);
+   const [remoteStream] = event.streams;
+    remoteVideoRef.current.srcObject = remoteStream
+    const v = remoteVideoRef.current as HTMLVideoElement;
+    v.muted = true; // or keep it muted in JSX for autoplay
+}
+ }
+ return(()=>{wsRef.current?.close();
+    rtcRef.current?.close();
+ });
+   },[])
+ 
+  return (
+   <div>
+    <h1>Reciever</h1>
+    <video ref={remoteVideoRef} autoPlay playsInline></video>
+   </div>
+  );
+}
---
+++ app/Sender/page.tsx
@@ -0,0 +1,75 @@
+"use client"
+import { useEffect ,useRef} from "react";
+
+
+export default function Sender() {
+ const wsRef  = useRef<WebSocket|null>(null);
+ const rtcRef = useRef<RTCPeerConnection|null>(null);
+  const localVideoRef = useRef<HTMLVideoElement|null>(null);
+   useEffect(()=>{
+     wsRef.current = new WebSocket('ws://192.168.1.6:8080')
+ 
+ wsRef.current.onopen= () =>{
+   wsRef.current?.send(JSON.stringify({type : 'sender'}));
+ };
+ wsRef.current.onmessage = (event:any) =>{
+  const message = JSON.parse(event.data);
+  if(message.type === 'createAnswer'){
+    console.log("Got answer");
+    rtcRef.current?.setRemoteDescription(message.sdp);
+  }
+  else if(message.type === 'iceCandidate'){
+    rtcRef.current?.addIceCandidate(new RTCIceCandidate(message.candidate));
+  }
+
+ }
+ return(()=>{wsRef.current?.close();
+    rtcRef.current?.close();
+ });
+   },[])
+ 
+
+async function initconnection(){
+  rtcRef.current = new RTCPeerConnection();
+  const pc = rtcRef.current
+  const ws = wsRef.current;
+  pc.onnegotiationneeded = async () =>{
+ const offer = await pc.createOffer();
+  pc.setLocalDescription(offer);
+  ws?.send(JSON.stringify({type :'createOffer', sdp : offer})); 
+  }
+
+   pc.onicecandidate = (event)=>{
+        if(event.candidate){
+          ws?.send(JSON.stringify({type:'iceCandidate', candidate:event.candidate}));
+        }
+      }
+ await startMedia();
+}
+
+const startMedia =async ()=>{
+try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        
+        // Add tracks to the connection
+        stream.getTracks().forEach((track) => {
+          rtcRef.current?.addTrack(track, stream);
+        });
+
+        // Attach to video element
+        if (localVideoRef.current) {
+          localVideoRef.current.srcObject = stream;
+        }
+      } catch (e) {
+        console.error("Error accessing media:", e);
+      }
+}
+
+  return (
+   <div>
+    <h1>Sender</h1>
+    <button onClick={initconnection}>Start</button>
+    <video ref={localVideoRef} autoPlay playsInline muted></video>
+   </div>
+  );
+}
